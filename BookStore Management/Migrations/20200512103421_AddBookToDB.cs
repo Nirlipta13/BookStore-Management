03617e38faf6a65c2928584d0c28b9325d3cd5cd
Nirlipta13 <nirliptadas13@gmail.com>
2020-05-12 16:23:24 +0530

    Added book model and js file for datatable

---
+++ BookStore Management/Migrations/20200512103421_AddBookToDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStore_Management.Migrations
+{
+    public partial class AddBookToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Author = table.Column<string>(nullable: true),
+                    ISBN = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
